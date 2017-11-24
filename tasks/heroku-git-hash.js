f60434bedbcf4ca5c0cd10c9473e25c2c8eae4a6
Julian Lepinski <lepinsk@users.noreply.github.com>
2017-11-24 13:55:36 -0800

    lil tab/space cleanup for readability
    🌚

--- tasks/heroku-git-hash.js
+++ tasks/heroku-git-hash.js
@@ -8,33 +8,33 @@ var yaml = require("js-yaml");
 var fs   = require("fs");
 
 module.exports = function(grunt) {
-	grunt.registerTask('heroku-git-hash', "Retrive the current Git hash on a Heroku deploy", function(){
+   grunt.registerTask('heroku-git-hash', "Retrive the current Git hash on a Heroku deploy", function(){
 
-		try {
+      try {
 
-			// born too late to explore the earth
-			// born too soon to explore the galaxy
-			// born just in time to BROWSE DANK FILESYSTEMS
-			var filePath = "/app/tmp/push_metadata.yml";
+         // born too late to explore the earth
+         // born too soon to explore the galaxy
+         // born just in time to BROWSE DANK FILESYSTEMS
+         var filePath = "/app/tmp/push_metadata.yml";
 
-			var pushMetadata = yaml.safeLoad(fs.readFileSync(filePath, "utf8"));
-			
-			if (pushMetadata.source_blob && pushMetadata.source_blob.version){
-				
-				var hashLong = pushMetadata.source_blob.version;
-				grunt.config.set("githash", hashLong);
-			
-			} else {
+         var pushMetadata = yaml.safeLoad(fs.readFileSync(filePath, "utf8"));
+         
+         if (pushMetadata.source_blob && pushMetadata.source_blob.version){
+            
+            var hashLong = pushMetadata.source_blob.version;
+            grunt.config.set("githash", hashLong);
+         
+         } else {
 
-				var issueURL = "https://github.com/lepinsk/grunt-heroku-git-hash/issues/1";
-				grunt.fail.warn("heroku-git-hash; push_metadata missing expected fields. see " + issueURL);
+            var issueURL = "https://github.com/lepinsk/grunt-heroku-git-hash/issues/1";
+            grunt.fail.warn("heroku-git-hash; push_metadata missing expected fields. see " + issueURL);
 
-			}
+         }
 
-		} catch (e) {
+      } catch (e) {
 
-			grunt.fail.warn("heroku-git-hash; error parsing yaml: " + e);
+         grunt.fail.warn("heroku-git-hash; error parsing yaml: " + e);
 
-		}
-	});
+      }
+   });
 };
